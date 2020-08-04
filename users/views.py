459d5a82a96465f5a6b4ebc842091fecc7da9fe6
Parthipan <parthij15@gmail.com>
2020-08-04 10:37:36 +0530

    Update views.py

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.exceptions import NotFound
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.response import Response
 
 from .models import User
