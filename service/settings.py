5a729192a1b2fb10b24a1a0ece9ee6d2aba570e2
Parthipan <parthij15@gmail.com>
2020-08-22 23:03:18 +0530

    Update settings.py

--- service/settings.py
+++ service/settings.py
@@ -23,7 +23,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', None)
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'develop')
 
